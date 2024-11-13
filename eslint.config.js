f1fae74d60f2cfe8dbaedf77d7aace731693ce34
Artemitol <artemiy.averin@gmail.com>
2024-11-13 21:43:49 +0300

    fixed bug with linting dist directory

--- eslint.config.js
+++ eslint.config.js
@@ -28,6 +28,7 @@ export default [
     eslintConfigPrettier,
     pluginReact.configs.flat.recommended,
     { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+    { ignores: ["node_modules", "dist"] },
     {
         plugins: {
             react: pluginReact,
@@ -68,6 +69,5 @@ export default [
                 },
             },
         },
-        ignores: ["node_modules", "dist"],
     },
 ]
