59b57a9f639d12792e02daecbd238b13ed770dae
Artemitol <artemiy.averin@gmail.com>
2024-11-27 09:53:45 +0300

    обновил шаблон

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -1,5 +1,4 @@
 import { HTMLAttributes, ReactNode } from "react"
-import classes from "./button.module.scss"
 
 type ButtonProps = {
     children: ReactNode
@@ -7,9 +6,5 @@ type ButtonProps = {
 }
 
 export function Button(props: ButtonProps & HTMLAttributes<HTMLButtonElement>) {
-    return (
-        <button className={classes.button} onClick={props.onClick}>
-            {props.children}
-        </button>
-    )
+    return <button onClick={props.onClick}>{props.children}</button>
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line import/no-default-export
+export default {
+    content: ["src/**/*/*{html, tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
